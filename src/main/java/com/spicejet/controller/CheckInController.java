2f415785891a57e88af9e206ea95f070677901d7
Aditya Chauhan <hi.aditya.chauhan@outlook.com>
2017-09-05 17:02:54 +0530

    lat long verify dummy service

--- src/main/java/com/spicejet/controller/CheckInController.java
+++ src/main/java/com/spicejet/controller/CheckInController.java
@@ -1,5 +1,7 @@
 package com.spicejet.controller;
 
+import com.spicejet.dto.PlaceVerification;
+import com.spicejet.dto.PlaceVerificationResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +13,29 @@ import com.spicejet.dto.User;
 public class CheckInController {
 
 	@RequestMapping(value= "/checkin", method = RequestMethod.POST)
-	void loginAuthentication(@RequestBody User user){
-		
-	}
+    PlaceVerificationResponse latLongVerify(@RequestBody PlaceVerification placeVerification){
+        PlaceVerificationResponse placeVerificationResponse=new PlaceVerificationResponse();
+        String latSpiceJet = "28.499904";
+        String longSpiceJet = "77.081778";
+        String latXebia = "28.459949";
+        String longXebia = "77.051589";
+
+        String lat = placeVerification.getLatitude();
+        String longi = placeVerification.getLongitude();
+        if(lat.equalsIgnoreCase(latSpiceJet) && longi.equalsIgnoreCase(longSpiceJet) ){
+            placeVerificationResponse.setStatus("CONFIRMED");
+            placeVerificationResponse.setHotelName("SPICEJET");
+            return placeVerificationResponse;
+        }
+        else if(lat.equalsIgnoreCase(latXebia) && longi.equalsIgnoreCase(longXebia) ) {
+            placeVerificationResponse.setStatus("CONFIRMED");
+            placeVerificationResponse.setHotelName("XEBIA");
+            return placeVerificationResponse;
+        }
+        else {
+            placeVerificationResponse.setStatus("COORDINATES DID NOT MATCH");
+            placeVerificationResponse.setHotelName("");
+            return placeVerificationResponse;
+        }
+    }
 }
---
+++ src/main/java/com/spicejet/dto/PlaceVerificationResponse.java
@@ -0,0 +1,23 @@
+package com.spicejet.dto;
+
+public class PlaceVerificationResponse {
+
+    String status;
+    String hotelName;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+}
