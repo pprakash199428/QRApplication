b8140b53397e39cb9e2ca15d31e13c38b08b3f0e
aditya_chauhan <achauhan@xebia.com>
2017-09-08 12:57:40 +0530

    distance between lats fix

--- src/main/java/com/spicejet/controller/CheckInController.java
+++ src/main/java/com/spicejet/controller/CheckInController.java
@@ -4,35 +4,42 @@ import com.spicejet.dto.PlaceVerification;
 import com.spicejet.dto.PlaceVerificationResponse;
 import org.springframework.web.bind.annotation.*;
 
-import com.spicejet.dto.User;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class CheckInController {
 
-	@RequestMapping(value= "/checkin", method = RequestMethod.POST)
-    PlaceVerificationResponse latLongVerify(@RequestBody PlaceVerification placeVerification){
-        PlaceVerificationResponse placeVerificationResponse=new PlaceVerificationResponse();
-        String latSpiceJet = "28.499904";
-        String longSpiceJet = "77.081778";
+    @RequestMapping(value = "/checkin", method = RequestMethod.POST)
+    PlaceVerificationResponse latLongVerify(@RequestBody PlaceVerification placeVerification) {
+        PlaceVerificationResponse placeVerificationResponse = new PlaceVerificationResponse();
+//        String latSpiceJet = "28.499904";
+//        String longSpiceJet = "77.081778";
         String latXebia = "28.459949";
         String longXebia = "77.051589";
 
         String lat = placeVerification.getLatitude();
         String longi = placeVerification.getLongitude();
-        if(lat.equalsIgnoreCase(latSpiceJet) && longi.equalsIgnoreCase(longSpiceJet) ){
+
+
+        double R = 6378.137;
+        double dLat = Double.parseDouble(lat) * Math.PI / 180 - Double.parseDouble(latXebia) * Math.PI / 180;
+        double dLon = Double.parseDouble(longi) * Math.PI / 180 - Double.parseDouble(longXebia) * Math.PI / 180;
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(Double.parseDouble(latXebia) * Math.PI / 180) * Math.cos(Double.parseDouble(lat) * Math.PI / 180) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+
+        System.out.println("saaaaaaaaaaaa"+d*1000);
+        if (d*1000 <= 50) {
             placeVerificationResponse.setStatus("CONFIRMED");
             placeVerificationResponse.setHotelName("SPICEJET");
             return placeVerificationResponse;
-        }
-        else if(lat.equalsIgnoreCase(latXebia) && longi.equalsIgnoreCase(longXebia) ) {
-            placeVerificationResponse.setStatus("CONFIRMED");
-            placeVerificationResponse.setHotelName("XEBIA");
-            return placeVerificationResponse;
-        }
-        else {
+        } else {
             placeVerificationResponse.setStatus("COORDINATES DID NOT MATCH");
             placeVerificationResponse.setHotelName("");
             return placeVerificationResponse;
         }
+
+
     }
 }
